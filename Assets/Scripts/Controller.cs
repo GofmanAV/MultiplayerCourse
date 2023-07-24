cdd667daeb29c5ac37e6e70829754dd5d93e08d2
GofmanAV <GofmanAV@yandex.ru>
2023-07-25 01:13:42 +0500

    week1

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter _player;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        _player.SetInput(h, v);
+
+        SendMove();
+    }
+
+    void SendMove()
+    {
+        _player.GetMoveInfo(out Vector3 position);
+        Dictionary<string, object> data = new Dictionary<string, object>()
+        {
+            { "x", position.x },
+            { "y", position.z }
+        };
+
+        MultiplayerManager.Instance.SendMessage("move", data);
+    }
+}
